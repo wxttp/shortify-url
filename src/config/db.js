ff7b5e50e7d3bf5328fab122d32f514322160f0d
w <winamonthep@gmail.com>
2025-06-30 22:11:05 +0700

    feat(db): add MongoDB connection and URL model
    - Add connectDB function to connect MongoDB
    - Create UrlModel with schema for originalUrl, shortUrl and createdAt fields

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+async function connectDB() {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
---
+++ src/models/UrlModel.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  originalUrl: {
+    type: String,
+    required: true,
+  },
+  shortUrl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UrlSchema = mongoose.model('url', urlSchema);
+
+export { UrlSchema };
